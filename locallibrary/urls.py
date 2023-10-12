78acbc42e40cd0d0a22bcd3e8f37830c915d1956
gr423_grvev <vlad.grishanenko.gg@gmail.com>
2023-10-12 10:00:46 +0700

    Practice

--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -4,30 +4,12 @@ from django.urls import path
 
 # Use include() to add URLS from the catalog application and authentication system
 from django.urls import include
+from django.views.generic import RedirectView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
-
-
-urlpatterns += [
     path('catalog/', include('catalog.urls')),
-]
-
-#
-# Use static() to add url mapping to serve static files during development (only)
-
-
-#Add URL maps to redirect the base URL to our application
-from django.views.generic import RedirectView
-urlpatterns += [
     path('', RedirectView.as_view(url='/catalog/', permanent=True)),
-]
-
-
-
-#Add Django site authentication urls (for login, logout, password management)
-urlpatterns += [
     path('accounts/', include('django.contrib.auth.urls')),
 ]
